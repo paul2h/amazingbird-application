e5c6be7157d100fdd1e35537c0a3d834e1f0c1d3
paul2h <paul2h@wavegis.com.tw>
2016-12-15 16:44:05 +0800

    branch調整

--- ApProgramBasicConstructor/src/com/kiwi/Starter.java
+++ ApProgramBasicConstructor/src/com/kiwi/Starter.java
@@ -17,7 +17,7 @@ import com.kiwi.global.GlobalConfig;
 
 public class Starter {
 
-	private static final String edition = "2017水情介接";
+	private static final String edition = "2017水情介接-二河局alpha";
 	private ApplicationContext context;
 	private Controller controller;
 
