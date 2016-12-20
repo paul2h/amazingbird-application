f84dbf11ad10e52bfa4d6c3ddddd20f9ac1b680d
paul2h <paul2h@wavegis.com.tw>
2016-12-20 14:41:35 +0800

    Engin測試用版本開Branch

--- ApProgramBasicConstructor/src/com/wavegis/Starter.java
+++ ApProgramBasicConstructor/src/com/wavegis/Starter.java
@@ -17,7 +17,7 @@ import com.wavegis.global.GlobalConfig;
 
 public class Starter {
 
-	private static final String edition = "2017水情介接";
+	private static final String edition = "2017水情介接-Engin測試用1.0版";
 	private ApplicationContext context;
 	private Controller controller;
 
