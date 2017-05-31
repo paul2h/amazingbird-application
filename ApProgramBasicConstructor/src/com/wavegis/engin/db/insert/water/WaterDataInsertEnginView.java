6fb17f9914b18dc57f7cb313a5a990123ea4bec0
paul2h <paul2h@wavegis.com.tw>
2017-05-31 16:07:43 +0800

    Water Data Insert Engin放入

---
+++ ApProgramBasicConstructor/src/com/wavegis/engin/db/insert/water/WaterDataInsertEnginView.java
@@ -0,0 +1,15 @@
+package com.wavegis.engin.db.insert.water;
+
+import com.wavegis.engin.prototype.SimpleEnginView;
+
+@SuppressWarnings("serial")
+public class WaterDataInsertEnginView extends SimpleEnginView {
+
+	private static final String enginID = "RawDataInsert";
+
+	@Override
+	public String getEnginID() {
+		return enginID;
+	}
+
+}
